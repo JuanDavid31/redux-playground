230dc0336573778fff4d228a5b5d8564fbfc84dc
Juan David <juandavid0306@hotmail.com>
2021-03-25 12:04:36 -0500

    Add counter using redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import myReducer from "./redux/myReducer";
+import {createStore} from "redux";
+
+const store = createStore(myReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
